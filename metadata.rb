816f5066ae3200b387d8ae1d6f85a5a75a5c2d71
Julian C. Dunn <jdunn@getchef.com>
2014-07-12 00:05:24 -0400

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "sysinternals"
-maintainer       "Opscode, Inc."
-maintainer_email "jdunn@opscode.com"
+maintainer       "Chef Software, Inc."
+maintainer_email "jdunn@getchef.com"
 license          "Apache 2.0"
 description      "Installs Microsoft SysInternals tools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
