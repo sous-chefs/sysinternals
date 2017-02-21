29b796160e8c44535474cdfbc76f7b32d45f05cd
Tim Smith <tsmith@chef.io>
2017-02-21 14:55:26 -0800

    Add new metadata and require Chef 12.6+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ license          "Apache 2.0"
 description      "Installs Microsoft SysInternals tools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.2"
+source_url       'https://github.com/chef-cookbooks/sysinternals'
+issues_url       'https://github.com/chef-cookbooks/sysinternals/issues'
+chef_version     '>= 12.6' if respond_to?(:chef_version)
