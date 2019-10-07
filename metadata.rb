8da596d0b1084e71400def8fc33322e2a79f3acf
Tim Smith <tsmith84@gmail.com>
2019-10-07 16:26:10 -0700

    Resolve cookstyle 5.8 warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,8 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs Microsoft SysInternals tools'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.1'
 supports 'windows'
 source_url       'https://github.com/chef-cookbooks/sysinternals'
 issues_url       'https://github.com/chef-cookbooks/sysinternals/issues'
-chef_version     '>= 12.6' if respond_to?(:chef_version)
+chef_version     '>= 12.6'
